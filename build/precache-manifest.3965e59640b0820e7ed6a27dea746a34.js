c8b7c4c5ffbf6b880e57d2753b630d9ad7f6e3ad
Vatsal Bhimani <vatsal.bhimani@georgebrown.ca>
2021-10-13 20:16:40 -0400

    build folder is added again

---
+++ build/precache-manifest.3965e59640b0820e7ed6a27dea746a34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a3ab0fdd7a3c68a45ec",
+    "url": "/vatsalbhimani03/comp3104/static/css/main.8784ef20.chunk.css"
+  },
+  {
+    "revision": "4a3ab0fdd7a3c68a45ec",
+    "url": "/vatsalbhimani03/comp3104/static/js/main.4a3ab0fd.chunk.js"
+  },
+  {
+    "revision": "baa3129cb2f3c81a2eb8",
+    "url": "/vatsalbhimani03/comp3104/static/js/runtime~main.baa3129c.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/vatsalbhimani03/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "c89226411360339ffd2e37e048b432bf",
+    "url": "/vatsalbhimani03/comp3104/index.html"
+  }
+];
